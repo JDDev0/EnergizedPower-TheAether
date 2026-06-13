623e7b8348897efc53461ffcf326cfec55d8d70a
JDDev0 <jddev0@dorneger.at>
2026-06-13 12:56:45 +0200

    Remove "HammerItemFix", because it is no longer needed

--- src/main/java/me/jddev0/epta/item/EPTAItems.java
+++ src/main/java/me/jddev0/epta/item/EPTAItems.java
@@ -3,6 +3,7 @@ package me.jddev0.epta.item;
 import com.aetherteam.aether.item.combat.AetherItemTiers;
 import com.aetherteam.aether.item.miscellaneous.bucket.SkyrootBucketItem;
 import me.jddev0.ep.fluid.EPFluids;
+import me.jddev0.ep.item.HammerItem;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
@@ -12,13 +13,13 @@ public final class EPTAItems {
     private EPTAItems() {}
     
     public static final Item SKYROOT_HAMMER = registerItem("skyroot_hammer",
-            new HammerItemFix(AetherItemTiers.SKYROOT, new Item.Properties()));
+            new HammerItem(AetherItemTiers.SKYROOT, new Item.Properties()));
     public static final Item HOLYSTONE_HAMMER = registerItem("holystone_hammer",
-            new HammerItemFix(AetherItemTiers.HOLYSTONE, new Item.Properties()));
+            new HammerItem(AetherItemTiers.HOLYSTONE, new Item.Properties()));
     public static final Item ZANITE_HAMMER = registerItem("zanite_hammer",
-            new HammerItemFix(AetherItemTiers.ZANITE, new Item.Properties()));
+            new HammerItem(AetherItemTiers.ZANITE, new Item.Properties()));
     public static final Item GRAVITITE_HAMMER = registerItem("gravitite_hammer",
-            new HammerItemFix(AetherItemTiers.GRAVITITE, new Item.Properties()));
+            new HammerItem(AetherItemTiers.GRAVITITE, new Item.Properties()));
 
     public static final Item SKYROOT_DIRTY_WATER_BUCKET = registerItem("skyroot_dirty_water_bucket",
             new SkyrootBucketItem(EPFluids.DIRTY_WATER, new Item.Properties().stacksTo(1)));
